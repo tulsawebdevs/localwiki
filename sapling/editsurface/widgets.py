fd519d362a69854cfbb4c9569b171e9df5750d03
Mike Ivanov <mivanov@gmail.com>
2011-12-15 10:26:41 -0800

    EditSurface widget that loads field value

---
+++ sapling/editsurface/widgets.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
+
+
+class EditSurface(forms.Textarea):
+    def render(self, name, value, attrs=None, **kwargs):
+        context = {
+            'value': value,
+        }
+        return mark_safe(render_to_string(
+            'editsurface/editsurface.html', context
+        ))
