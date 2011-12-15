e4f8829a900d17b4cae0c3602ac77b89462b8a6b
Mike Ivanov <mivanov@gmail.com>
2011-12-15 10:51:24 -0800

    Allow saving after editing

--- sapling/editsurface/widgets.py
+++ sapling/editsurface/widgets.py
@@ -3,11 +3,13 @@ from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 
 
-class EditSurface(forms.Textarea):
+class EditSurface(forms.HiddenInput):
     def render(self, name, value, attrs=None, **kwargs):
+        rendered = super(EditSurface, self).render(name, value, attrs)
         context = {
+            'name': name,
             'value': value,
         }
-        return mark_safe(render_to_string(
+        return rendered + mark_safe(render_to_string(
             'editsurface/editsurface.html', context
         ))
